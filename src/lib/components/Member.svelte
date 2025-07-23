428f9e5845584fe9c0614912b644143c3ae2756f
Phillip Malboeuf <phil@phils.computer>
2025-07-23 12:54:13 -0400

    card flip

--- src/lib/components/Member.svelte
+++ src/lib/components/Member.svelte
@@ -9,56 +9,72 @@
 </script>
 
 <section class="member">
-  
-  <figure>
-    {#if item.fields.photo}
-    <Media media={item.fields.photo} />
-    {/if}
-  </figure>
-
-  {#if item.fields.bio}
-  <div class="flex flex--column flex--gapped flex--spaced bio">
-    {#if item.fields.titre}
-    <div class="col">
-      <h5>
-        {item.fields.prenom} {item.fields.nom}<br>
-        <em>{item.fields.titre}</em>
-      </h5>
-    </div>
-    {/if}
+  <div class="card">
+    <figure class="photo">
+      {#if item.fields.photo}
+      <Media media={item.fields.photo} />
+      {/if}
+    </figure>
 
     {#if item.fields.bio}
-    <Rich body={item.fields.bio} />
-    {/if}
-
-    <div class="flex flex--gapped flex--spaced flex--end">
-      {#if item.fields.membre}
-      <div>
-        <small>Membre</small><br>
-        <strong>{item.fields.membre}</strong>
+    <div class="flex flex--column flex--gapped flex--spaced bio">
+      {#if item.fields.titre}
+      <div class="col">
+        <h5>
+          {item.fields.prenom} {item.fields.nom}<br>
+          <em>{item.fields.titre}</em>
+        </h5>
       </div>
       {/if}
-      {#if item.fields.linkedIn}
-      <a href={item.fields.linkedIn} class="button" target="_blank">LinkedIn</a>
+
+      {#if item.fields.bio}
+      <Rich body={item.fields.bio} />
       {/if}
+
+      <div class="flex flex--gapped flex--spaced flex--end">
+        {#if item.fields.membre}
+        <div>
+          <small>Membre</small><br>
+          <strong>{item.fields.membre}</strong>
+        </div>
+        {/if}
+        {#if item.fields.linkedIn}
+        <a href={item.fields.linkedIn} class="button" target="_blank">LinkedIn</a>
+        {/if}
+      </div>
     </div>
+    {/if}
   </div>
-  {/if}
 </section>
 
 <style lang="scss">
   .member {
     position: relative;
-    border-radius: $radius;
-    overflow: hidden;
+    perspective: 2000px;
+
+    .card {
+      border-radius: $radius;
+      transition: transform 0.666s, box-shadow 0.666s;
+      transform-style: preserve-3d;
+    }
 
     &:hover,
     &:focus-visible {
-      box-shadow: 8px 4px 15px 0px rgba(0, 0, 0, 0.10);
+      z-index: 2;
 
-      .bio {
-        opacity: 1;
+      .card {
+        transform: rotateY(-180deg);
+        box-shadow: -8px 4px 15px 0px rgba(0, 0, 0, 0.10);
       }
+    }
+
+    .photo,
+    .bio {
+      position: relative;
+      backface-visibility: hidden;
+      transform: rotateY(0deg);
+      border-radius: $radius;
+      overflow: hidden;
     }
 
     .bio {
@@ -68,9 +84,8 @@
       width: 100%;
       height: 100%;
       background-color: $blanc;
-      opacity: 0;
-      transition: opacity 0.333s;
       padding: $s0;
+      transform: rotateY(180deg);
 
       h5 {
         em {
