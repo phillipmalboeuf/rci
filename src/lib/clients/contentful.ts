08f657c7c984bea1ba0d5f57b6c132653cbc3fa8
Phillip Malboeuf <phil@phils.computer>
2025-08-23 14:03:03 -0400

    preview

--- src/lib/clients/contentful.ts
+++ src/lib/clients/contentful.ts
@@ -1,8 +1,8 @@
-// import { PREVIEW } from '$env/static/private'
+import { PREVIEW } from '$env/static/private'
 import { dev } from '$app/environment'
 import { createClient } from 'contentful'
 
-const preview = !!dev
+const preview = !!PREVIEW
 export const content = createClient({
   space: 'xq8w9oa8sl49',
   host: preview ? 'preview.contentful.com' : 'cdn.contentful.com',
