04ee3c70b5216fc6a8084e1d0995b9e4ea58dd0d
Phillip Malboeuf <phil@phils.computer>
2026-01-28 15:13:50 -0500

    vercel

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,16 @@
 import type { Handle } from '@sveltejs/kit';
+import { sequence } from "@sveltejs/kit/hooks"
 import { paraglideMiddleware } from '$lib/paraglide/server';
+
+export const cache: Handle = async ({ event, resolve }) => {
+
+	const response = await resolve(event);
+
+  response.headers.set('Vercel-CDN-Cache-Control', 'max-age=3600000');
+  response.headers.set('Vercel-Cache-Tag', process.env.VERCEL_URL || 'dev');
+
+	return response;
+};
 
 const handleParaglide: Handle = ({ event, resolve }) => paraglideMiddleware(event.request, ({ request, locale }) => {
 	event.request = request;
@@ -9,4 +20,4 @@ const handleParaglide: Handle = ({ event, resolve }) => paraglideMiddleware(even
 	});
 });
 
-export const handle: Handle = handleParaglide;
+export const handle: Handle = sequence(handleParaglide, cache);
